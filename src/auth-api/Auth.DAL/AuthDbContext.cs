b984fdb8c987e1f2b7b0183eb5362b24606ae85b
Adrian W <arterius169@gmail.com>
2023-06-26 19:48:10 +0200

    auth api initial

---
+++ src/auth-api/Auth.DAL/AuthDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Auth.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.DAL;
+public class AuthDbContext : DbContext
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<User> Users { get; set; }
+}
